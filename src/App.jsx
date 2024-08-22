9b84b8fe96d3268f262dcd58637fe172b6467d63
Viviana <acero.vivianapulido@gmail.com>
2024-08-22 11:17:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="underline text-3xl font-bold">
+      hola mundo
+      </h1>
+
+    </>
+  )
+}
+
+export default App
