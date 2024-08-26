240c3d1d67b86192e50d9f021de3ec79ed950807
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-08-26 10:07:22 +0200

    DONE: create component login TODO: Styles login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import './index.css'
-import { Button } from "@/components/ui/button"
+import Login from './components/login/login';
+import './index.css';
 
 function App() {
-
+  
   return (
     <>
-      <h1 className="underline text-3xl font-bold">
-      hola mundo
-      </h1>
-      
-      <Button>Click me</Button>
-
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
